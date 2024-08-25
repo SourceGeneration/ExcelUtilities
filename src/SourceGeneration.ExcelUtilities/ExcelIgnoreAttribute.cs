b262b23b375974ad6f3e0c2892909fd8b13ea01b
肖昕 <xiao.xin@outlook.com>
2024-08-25 23:19:09 +0800

    Add IgnoreAttribute

---
+++ src/SourceGeneration.ExcelUtilities/ExcelIgnoreAttribute.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace SourceGeneration.ExcelUtilities;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field,AllowMultiple = false,Inherited = true)]
+public sealed class ExcelIgnoreAttribute : Attribute { }
